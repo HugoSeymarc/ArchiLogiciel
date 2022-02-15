8aa58dc12dc4af9b0907b242bc0e5b2ef5fe6928
HugoSeymarc <hseymarc1@gmail.com>
2022-02-15 14:29:51 +0100

    Error POST

--- atelierI/js/fetch.js
+++ atelierI/js/fetch.js
@@ -22,26 +22,43 @@ function err_callback(error) {
 function createNewCard() {
     console.log("onClick -> OK");
 
+    var data = {
+        "name": document.getElementsByName("name")[0].value,
+        "description": document.getElementsByName("description")[0].value,
+        "family": document.getElementsByName("family")[0].value,
+        "affinity": document.getElementsByName("affinity")[0].value,
+        "imgUrl": document.getElementsByName("imgUrl")[0].value,
+        "energy": document.getElementsByName("energy")[0].value,
+        "hp": document.getElementsByName("hp")[0].value,
+        "defence": document.getElementsByName("defence")[0].value,
+        "attack": document.getElementsByName("attack")[0].value,
+        "price": document.getElementsByName("price")[0].value,
+        "userId": null,
+        "storeId": null
+    }
+
     const POST_CARD_URL = "https://asi2-backend-market.herokuapp.com/card";
+
     let context = {
         method: 'POST',
-        body: JSON.stringify({
-            "name": document.getElementsByName("name")[0].value,
-            "description": document.getElementsByName("description")[0].value,
-            "family": document.getElementsByName("family")[0].value,
-            "affinity": document.getElementsByName("affinity")[0].value,
-            "imgUrl": document.getElementsByName("imgUrl")[0].value,
-            "energy": document.getElementsByName("energy")[0].value,
-            "hp": document.getElementsByName("hp")[0].value,
-            "defence": document.getElementsByName("defence")[0].value,
-            "attack": document.getElementsByName("attack")[0].value,
-            "price": document.getElementsByName("price")[0].value
-        })
+        body: JSON.stringify(data)
     };
 
-
     fetch(POST_CARD_URL, context)
-        .then(data => console.log("Post OK", data))
-        .catch(error => err_callback(error));
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data)
+        });
+
+    console.log(data)
+
+
+
+
+
+
+
 
 }
