30dc8ef8f43f6f2da4c224d16c9209f064b57cd9
HugoSeymarc <hseymarc1@gmail.com>
2022-02-15 13:20:58 +0100

    ADD: GET method statique

---
+++ atelierI/js/card.js
@@ -0,0 +1,46 @@
+const GET_CARD_URL = "https://asi2-backend-market.herokuapp.com/cards";
+let context = {
+    method: 'GET'
+};
+
+fetch(GET_CARD_URL, context)
+    .then(data => data.json())
+    .then(datajson => callback(datajson))
+    .catch(error => err_callback(error));
+
+function err_callback(error) {
+    console.log(error);
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function callback(data) {
+
+    let template = document.querySelector("#selectedCard");
+
+    let button = "buy";
+
+    for (const card of data) {
+
+        comment = getRandomInt(1000);
+        like = getRandomInt(1000);
+        date = getRandomInt(250) + "h";
+
+        let clone = document.importNode(template.content, true);
+
+        newContent = clone.firstElementChild.innerHTML
+            .replace(/{{family_src}}/g, card.smallImgUrl)
+            .replace(/{{family_name}}/g, card.name)
+            .replace(/{{image_src}}/g, card.imgUrl)
+            .replace(/{{date}}/g, date)
+            .replace(/{{comment}}/g, comment)
+            .replace(/{{like}}/g, like)
+            .replace(/{{button}}/g, button);
+        clone.firstElementChild.innerHTML = newContent;
+
+        let cardContainer = document.querySelector("#gridContainer");
+        cardContainer.appendChild(clone);
+    }
+}
---
+++ atelierI/js/fetch.js
@@ -0,0 +1,47 @@
+// function generate() {
+
+//     const GET_CHUCK_URL = "https://api.chucknorris.io/jokes/random";
+//     let context = {
+//         method: 'GET'
+//     };
+
+//     fetch(GET_CHUCK_URL, context)
+//         .then(response => response.json())
+//         .then(response => callback(response))
+//         .catch(error => err_callback(error));
+// }
+
+// function callback(response) {
+//     document.getElementById("txtChuck").innerHTML = response.value;
+// }
+
+function err_callback(error) {
+    console.log(error);
+}
+
+function createNewCard() {
+    console.log("onClick -> OK");
+
+    const POST_CARD_URL = "https://asi2-backend-market.herokuapp.com/card";
+    let context = {
+        method: 'POST',
+        body: JSON.stringify({
+            "name": document.getElementsByName("name")[0].value,
+            "description": document.getElementsByName("description")[0].value,
+            "family": document.getElementsByName("family")[0].value,
+            "affinity": document.getElementsByName("affinity")[0].value,
+            "imgUrl": document.getElementsByName("imgUrl")[0].value,
+            "energy": document.getElementsByName("energy")[0].value,
+            "hp": document.getElementsByName("hp")[0].value,
+            "defence": document.getElementsByName("defence")[0].value,
+            "attack": document.getElementsByName("attack")[0].value,
+            "price": document.getElementsByName("price")[0].value
+        })
+    };
+
+
+    fetch(POST_CARD_URL, context)
+        .then(data => console.log("Post OK", data))
+        .catch(error => err_callback(error));
+
+}
