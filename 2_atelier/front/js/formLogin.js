662752d744088ca69de3826a56af99267b9847bb
leoM26 <leoalbouy26@gmail.com>
2022-03-15 14:49:35 +0100

    fix: POST Login

--- 2_atelier/front/js/formLogin.js
+++ 2_atelier/front/js/formLogin.js
@@ -1,28 +1,31 @@
-const URL_API_BDD = "http://127.0.0.1:8080/login";
+const URL_API_BDD = `http://127.0.0.1:8080/login`;
 
-function err_callback(error) {
-    console.log(error);
-}
 
 function userLogin() {
     console.log("login -> Clicked");
-
+    
     var data = {
-        "userName": document.getElementsByName("login")[0].value,
+        "userName": document.getElementsByName("userName")[0].value,
         "password": document.getElementsByName("password")[0].value
     }
-
+    
     let context = {
         method: 'POST',
-        mode: 'no-cors',
-        body: JSON.stringify(data)
+        body: JSON.stringify(data),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8'
+        }
     };
-
-
+    
+    
     fetch(URL_API_BDD, context)
-        .then(data => data.json())
-        .then(datajson => callback(datajson))
-        .catch(error => err_callback(error));
-
+    .then(data => data.json())
+    .then(datajson => console.log(datajson))
+    .catch(error => err_callback(error));
+    
     console.log(data)
+}
+
+function err_callback(error) {
+    console.log(error);
 }
