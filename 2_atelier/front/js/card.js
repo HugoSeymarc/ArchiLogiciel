46f959e29d68bcab666112c0315f993a33a08f99
HugoSeymarc <hseymarc1@gmail.com>
2022-03-15 10:25:11 +0100

    add: front

---
+++ 2_atelier/front/js/card.js
@@ -0,0 +1,49 @@
+const URL_API_GET = "https://asi2-backend-market.herokuapp.com/cards";
+let context = {
+    method: 'GET'
+};
+
+fetch(URL_API_GET, context)
+    .then(data => data.json())
+    .then(datajson => callback(datajson))
+    .catch(error => err_callback(error));
+
+function err_callback(error) {
+    console.log(error);
+}
+
+function getRandomInt(max) { // DOnne un nombre aleatoire
+    return Math.floor(Math.random() * max);
+}
+
+
+function callback(data) {
+
+    let template = document.querySelector("#selectedCard");
+
+    let button = "buy";
+
+    for (const card of data) {
+
+        // console.log(card)
+        like = getRandomInt(1000);
+
+        let clone = document.importNode(template.content, true);
+
+        newContent = clone.firstElementChild.innerHTML
+            .replace(/{{energy}}/g, card.energy)
+            .replace(/{{name}}/g, card.name)
+            .replace(/{{imgUrl}}/g, card.imgUrl)
+            .replace(/{{description}}/g, card.description)
+            .replace(/{{energy}}/g, card.energy)
+            .replace(/{{hp}}/g, card.hp)
+            .replace(/{{defence}}/g, card.defence)
+            .replace(/{{attack}}/g, card.attack)
+            .replace(/{{price}}/g, card.price)
+            .replace(/{{like}}/g, like);
+        clone.firstElementChild.innerHTML = newContent;
+
+        let cardContainer = document.querySelector("#gridContainer");
+        cardContainer.appendChild(clone);
+    }
+}
