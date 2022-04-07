dc78aae44015077b433569f409319063c94424a3
leoM26 <leoalbouy26@gmail.com>
2022-04-07 11:12:32 +0200

    fix: button

--- 3_atelier/front/src/pages/Market.js
+++ 3_atelier/front/src/pages/Market.js
@@ -12,7 +12,7 @@ const Market = () => {
             <Row className={"justify-content-around"}>
                 <Button title={"BUY"} href={`/buy`}  color={"light"}  width={"25%"} height={"200px"} icon={process.env.PUBLIC_URL + "./logo192.png"} iconWidth={"15%"}/>
                 <Button title={"SELL"} href={`/sell`} color={"light"} width={"25%"} height={"200px"} icon={process.env.PUBLIC_URL + "./logo192.png"} iconWidth={"15%"}/>
-                <Button title={"PLAY"} href={`/game`} color={"light"} width={"25%"} height={"200px"} icon={process.env.PUBLIC_URL + "./logo192.png"} iconWidth={"15%"}/>
+                <Button title={"PLAY"} href={`/play`} color={"light"} width={"25%"} height={"200px"} icon={process.env.PUBLIC_URL + "./logo192.png"} iconWidth={"15%"}/>
             </Row>
         </>
     );
