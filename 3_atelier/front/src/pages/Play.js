3055104649c3e2442c99ca577490e8929818aad9
leoM26 <leoalbouy26@gmail.com>
2022-04-07 10:49:50 +0200

    feat: modal

--- 3_atelier/front/src/pages/Play.js
+++ 3_atelier/front/src/pages/Play.js
@@ -1,18 +1,57 @@
 import React from 'react';
-import Button from '../components/Button';
 import Table from '../components/Table';
+import { Modal, Button, Form } from 'react-bootstrap';
 
 const headData = ["Room", "User", "Bet", "GO"]
-const bodyData = [{"id":"A", "room":"Steph", "bet":"350"}, {"id":"B", "room":"Antoine", "bet":"50"},{"id":"C", "room":"Jackson", "bet":"150"},{"id":"D", "room":"Peter", "bet":"250"}]
+const bodyData = [{ "id": "A", "room": "Steph", "bet": "350" }, { "id": "B", "room": "Antoine", "bet": "50" }, { "id": "C", "room": "Jackson", "bet": "150" }, { "id": "D", "room": "Peter", "bet": "250" }]
 
-console.log(bodyData)
+const MyVerticallyCenteredModal = (props) => {
+    return (
+        <Modal
+            {...props}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+            <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter">
+                    Room creation
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form.Label htmlFor="name">Name</Form.Label>
+                <Form.Control
+                    type="text"
+                    id="name"
+                />
+                <Form.Label htmlFor="bet">Bet</Form.Label>
+                <Form.Control
+                    type="bet"
+                    id="bet"
+                />
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="primary" type="submit">
+                    Create
+                </Button>
+                <Button onClick={props.onHide}>Cancel</Button>
+            </Modal.Footer>
+        </Modal>
+    );
+}
+
 const Market = () => {
+    const [modalShow, setModalShow] = React.useState(false);
 
     return (
         <>
             <p>Market</p>
-            <Table head={headData} body={bodyData}/>
-            <Button title={"Create Room"} href={`/game`} color={"light"} width={"15%"} height={"350px"} icon={process.env.PUBLIC_URL + "./logo192.png"} iconWidth={"15%"}/>            
+            <Table head={headData} body={bodyData} />
+            <Button variant="primary" onClick={() => setModalShow(true)}>Create Room</Button>
+            <MyVerticallyCenteredModal
+                show={modalShow}
+                onHide={() => setModalShow(false)}
+            />
         </>
     );
 };
