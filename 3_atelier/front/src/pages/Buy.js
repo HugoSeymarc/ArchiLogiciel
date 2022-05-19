bd0ad67171d7fdb880c090bab7ee6cfcf1779ec8
HugoSeymarc <hseymarc1@gmail.com>
2022-05-19 19:57:02 +0200

    buy

--- 3_atelier/front/src/pages/Buy.js
+++ 3_atelier/front/src/pages/Buy.js
@@ -44,9 +44,8 @@ const Buy = () => {
 
     return (
         <>
-            <Header title="Buy"/>
+            <Header title="Market"/>
             <Table head={headData} body={bodyData} />
-            <Button variant="primary" onClick={() => setModalShow(true)}>Create Room</Button>
         </>
     );
 };
